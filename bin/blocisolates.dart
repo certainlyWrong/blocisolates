4834f3f28a9afdf543f3c8f33c4e7394a944672f
Adriano R. de Sousa <adrianorodriguesdsousa@ufpi.edu.br>
2024-08-14 10:29:12 -0300

    Initial setup

---
+++ bin/blocisolates.dart
@@ -0,0 +1,5 @@
+import 'package:blocisolates/blocisolates.dart' as blocisolates;
+
+void main(List<String> arguments) {
+  print('Hello world: ${blocisolates.calculate()}!');
+}
---
+++ test/blocisolates_test.dart
@@ -0,0 +1,8 @@
+import 'package:blocisolates/blocisolates.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
